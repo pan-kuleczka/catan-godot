c699291c79f464c2be03c831301fa7e9921583c8
Jeremiasz Preiss <80748387+pan-kuleczka@users.noreply.github.com>
2025-02-16 02:02:10 +0100

    Basic inventory features, basic card animations

--- cards/resource_card.gd
+++ cards/resource_card.gd
@@ -8,8 +8,8 @@ func _init(_resource : int) -> void:
 
 func get_node() -> Node:
 	var card_scene : PackedScene = preload("res://cards/scenes/card_scene.tscn")
-	var texture_path : String = "res://assets/cards/" + GameResource.e.keys()[resource] + ".svg"
+	var texture_path : String = "res://assets/cards/" + GameResource.e.keys()[resource].to_lower() + ".svg"
 	if FileAccess.file_exists(texture_path):
 		return card_scene.instantiate().with_data(load(texture_path))
-	push_warning("Card with resource " + str(resource) + " does not have a texture, falling back to a default one")
+	push_warning("Card with resource " + str(resource) + " does not have a texture at" + texture_path + ", falling back to a default one")
 	return card_scene.instantiate()
--- cards/scenes/card_inventory_scene.gd
+++ cards/scenes/card_inventory_scene.gd
@@ -2,22 +2,42 @@ extends Control
 
 var inventory : CardInventory
 
-@onready var CardContainer : HBoxContainer = $"CardContainer"
+@onready var CardsNode : Control = $"Cards"
 
 func _ready() -> void:
 	inventory = CardInventory.new()
 
+func calculate_final_card_positions(n_cards : int) -> Array[Vector2]:
+	var rect : Rect2 = CardsNode.get_rect()
+	var center : Vector2 = rect.get_center()
+	var max_cards_per_side : int = n_cards / 2
+	var card_width : float = 200 # TODO: This should be done better...
+	var offset : float = max((rect.size.x - card_width) / (2 * max_cards_per_side), 0)
+	var left_pos : Vector2 = center - Vector2(max_cards_per_side * offset, 0)
+	var positions : Array[Vector2] = []
+	for i in range(n_cards):
+		positions.append(left_pos + Vector2(offset, 0) * i)
+	return positions
+	
 func draw_cards() -> void:
 	# Remove all cards from the scene
-	for child : Node in CardContainer.get_children():
+	for child : Node in CardsNode.get_children():
 		child.queue_free()
-		CardContainer.remove_child(child)
-		
+		CardsNode.remove_child(child)
+	
+	var cards : Array[Card] = inventory.get_cards()
+	var positions : Array[Vector2] = calculate_final_card_positions(len(cards))
+	
 	# Add all cards to the scene
-	for card : Card in inventory.get_cards():
+	for i in range(len(cards)):
+		var card : Card = cards[i]
 		var card_node : Node = card.get_node()
-		CardContainer.add_child(card_node)
+		card_node.position = positions[i]
+		CardsNode.add_child(card_node)
 		
 func add_card(card : Card) -> void:
 	inventory.add_card(card)
 	draw_cards()
+
+func _on_button_pressed() -> void:
+	add_card(ResourceCard.new(randi() % 5 + 1))
